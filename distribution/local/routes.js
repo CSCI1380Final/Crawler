ad85694f4f83fc699a254b3786b4940c69ecf7f3
ZejunZhou <zzhou443>
2025-02-21 17:55:08 +0000

    update routes

--- distribution/local/routes.js
+++ distribution/local/routes.js
@@ -14,14 +14,38 @@ function get(configuration, callback) {
         console.log(v)
         }
    };
-  if (typeof configuration !== "string") {
-    return callback(new Error("Service must be a string"), null);
+    let service
+    let gid
+    if (configuration instanceof Object) {
+      service = configuration.service;
+      gid = configuration.gid || "local"; // by default it should consider local
+    } else {
+      service = configuration;
+      gid = "local";
+    }
+    let calledService;
+  // forward to local
+  if (gid === "local") {
+    if (service in routes) {
+      calledService = routes[service];
+    }
+  // forward to other nodes
+  } else {
+    if (
+      global.distribution &&
+      global.distribution[gid] &&
+      service in global.distribution[gid]
+    ) {
+      calledService = global.distribution[gid][service];
+    }
   }
-  if (!(configuration in routes)) {
-    return callback(new Error(`Service "${configuration}" is not found`), null);
+  if (!calledService) {
+    return callback(
+      new Error(`service "${service}" not found in group "${gid}"`),
+      null
+    );
   }
-
-  return callback(null, routes[configuration]);
+  return callback(null, calledService);
 }
 
 
